4c9aa6df96fdf31496bd1aa9576b71bec588d656
Ishan1210 <ishangrover121005@gmail.com>
2025-04-02 11:28:29 +0530

    node js

---
+++ script2.js
@@ -0,0 +1,27 @@
+// function sayHello() {
+//     let you = prompt('What is your name?');
+//    console.log('Hello', you + '!');
+// }
+// sayHello();
+
+// let varContainingFunction = function() {
+//     let varInFunction = "I'm a variable in a function";
+//     console.log("hi There!",varInFunction);
+// };
+
+// varContainingFunction();
+
+// function tester(para1, para2) {
+//     return para1 + " " + para2;
+// }
+// const test = tester("Hello", "World!");
+// console.log(test);
+
+function add(a, b) {
+    console.log(a+b);
+}
+parseInt(prompt("Enter first number: ", function(a)) {
+    parseInt(prompt("Enter second number: ", function(b))) {
+        add(a, b);
+    }; 
+}
