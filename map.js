2de4bc145b8d45bab0b03bcd524f0f0da3fd2a05
Ishan1210 <ishangrover121005@gmail.com>
2025-04-23 11:29:58 +0530

    Maps - 23/4/25

---
+++ map.js
@@ -0,0 +1,56 @@
+// const fruits = new Map([
+//     ['apple', 500],
+//     ['banana', 300],
+//     ['orange', 200],
+// ]);
+// console.log(fruits);
+
+// const fruits2 = new Map();
+// fruits2.set('apple', 500);
+// fruits2.set('banana', 300);
+// fruits2.set('orange', 200);
+//  console.log(fruits2);
+
+// console.log(fruits.get('banana'));
+
+//JS Map Methods
+const fruits = new Map([
+    ['apple', 500],
+    ['banana', 300],
+    ['orange', 200],
+]);
+// console.log(fruits.size);
+// console.log(fruits.delete('apple'));
+// console.log(fruits);
+// console.log(fruits.has('banana'));
+// console.log(fruits.clear());
+// let text="";
+// fruits.forEach((value, key) => {
+//     text += key + " " + value + "\n";
+// });
+// console.log(text);
+// console.log(typeof text);
+
+// let text = "";
+// for(const x of fruits.entries()){
+//     text += x + "\n";
+// }
+// console.log(text);
+
+// let text = "";
+// for(const x of fruits.keys()){
+
+//     text += x + "\n";
+// }
+// console.log(text);
+
+// let text = "";
+// for(const x of fruits.values()){
+
+//     text += x + "\n";
+// }
+// console.log(text);
+
+
+
+
